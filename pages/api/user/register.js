855965081f077e3ad20015d2de705b8ee6f2c7a8
josaroj <josaroj3619371@gmail.com>
2021-12-27 00:06:15 +0700

    211226_2359

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       [user_username]
     );
 
-    //console.log(results);
+    console.log(results);
     const data = results.data[0];
     if (data) {
       res.status(401).json({ status: { code: 401, message: "Unauthorized : Repeated username" } });
