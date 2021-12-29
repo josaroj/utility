54c079bff6afc4cfa2d480b5af260b85dd299485
josaroj <josaroj3619371@gmail.com>
2021-12-29 22:31:06 +0700

    211229_0906

--- pages/api/meterdata/main.js
+++ pages/api/meterdata/main.js
@@ -5,6 +5,7 @@ import { verifyToken } from "../../../lib/auth";
 export default async function handler(req, res){
     if(req.method === "GET"){
         const token = req.headers.authorization;
+        console.log(token);
         const verifiedToken = verifyToken(token);
         if(verifiedToken){
             const results0 = await query("SELECT * FROM meterdata ORDER BY DATE_FORMAT(date, '%Y%m%d') DESC LIMIT 1");
