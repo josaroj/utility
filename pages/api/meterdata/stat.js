96d13a11d81daba573d024377723d70890fab6b2
josaroj <josaroj3619371@gmail.com>
2021-12-29 23:20:01 +0700

    211229_0906

--- pages/api/meterdata/stat.js
+++ pages/api/meterdata/stat.js
@@ -8,8 +8,8 @@ export default async function handler(req, res){
         if(verifiedToken){
             // console.log(req);
             console.log(req.body);
-            const {fromdate, todate} = req.body;
-            const results = await query("SELECT * FROM meterdata WHERE date BETWEEN ? AND ?", [fromdate, todate]);
+            const {token, d1, d2} = req.body;
+            const results = await query("SELECT * FROM meterdata WHERE date BETWEEN ? AND ?", [d1, d2]);
             // console.log(results);
             res.status(results.status.code).json(results);
         }else{
