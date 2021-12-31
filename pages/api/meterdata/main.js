035bf62e82ba8f46f67d2ecbbdd42ae9c6cce545
josaroj <josaroj3619371@gmail.com>
2021-12-31 17:44:11 +0700

    311231_1726

--- pages/api/meterdata/main.js
+++ pages/api/meterdata/main.js
@@ -8,7 +8,7 @@ export default async function handler(req, res){
         console.log(token);
         const verifiedToken = verifyToken(token);
         if(verifiedToken){
-            const results0 = await query("SELECT * FROM meterdata WHERE MAX(time) IN (SELECT MAX(date) FROM meterdata");
+            const results0 = await query("SELECT * FROM meterdata WHERE MAX(time) IN (SELECT MAX(date) FROM meterdata)");
             // const results0 = await query("SELECT * FROM meterdata ORDER BY DATE_FORMAT(date, '%Y%m%d') DESC LIMIT 1");
             console.log(results0);
             const LastDate = results0.data[0].date;
